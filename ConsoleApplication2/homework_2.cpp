13c60d1eb4e8edb3dde5f7ffc39e4a82266b5734
Ilya Ivanov <malycheharovaleksey@gmail.com>
2022-05-25 08:57:39 +0900

    version_3

---
+++ ConsoleApplication2/homework_2.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int a = 150;
+    float b = 15.933;
+    char c = '250';
+    std::cout << "a = " << a << "\n";
+    std::cout << "b = " << b << "\n";
+    std::cout << "c = " << c << "\n";
+    int d = 15;
+    std::string m = "Июля";
+    int y = 2004;
+    std::cout << "Моя дата рождения: " << d << " " << m << " " << y << " года" << "\n";
+    const float f = 2.3;
+    const std::string s = "WINDOWS";
+    std::cout << f << " " << s << "\n";
+}
+
